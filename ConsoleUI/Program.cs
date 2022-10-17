9cc3b861e51dad413373132a8a7cde25f7b75a38
Ömer Çağrı Bozdemir <cagribozdemir0@gmail.com>
2022-10-17 22:15:00 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategory(int categoryId);
+    }
+}
