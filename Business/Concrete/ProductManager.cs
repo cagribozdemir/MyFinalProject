9aae6d4dee892120e9597b36af98d87cae60f960
Ömer Çağrı Bozdemir <cagribozdemir0@gmail.com>
2022-10-25 16:39:30 +0300

    Asp.Net API

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -39,7 +39,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour==4)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
---
+++ WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container
+//AOP
+builder.Services.AddSingleton<IProductService, ProductManager>();
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
